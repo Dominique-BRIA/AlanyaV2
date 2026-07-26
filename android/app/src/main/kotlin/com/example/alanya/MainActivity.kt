ae0556ad9bb1ec6e01580524655b147ba08516e2
Dominique-Bria <d.bria00@gmail.com>
2026-07-26 13:19:27 +0100

    delete all

--- android/app/src/main/kotlin/com/example/alanya/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.alanya
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/sewachat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sewachat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
